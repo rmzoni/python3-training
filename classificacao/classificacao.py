e9d37ec1b7ab9eb680f4e8113d746fc2fc832251
rmzoni <rmzoni@gmail.com>
2017-08-17 00:13:42 -0300

    Comecando algoritmo de classificacao

---
+++ classificacao/classificacao.py
@@ -0,0 +1,23 @@
+# [e gordinho?, tem perninha curta?, faz auau?]
+porco1 = [1, 1, 0]
+porco2 = [1, 1, 0]
+porco3 = [1, 1, 0]
+cachorro4 = [1, 1, 1]
+cachorro5 = [0, 1, 1]
+cachorro6 = [0, 1, 1]
+
+dados = [porco1, porco2, porco3, cachorro4, cachorro5, cachorro6]
+
+marcacoes = [1, 1, 1, -1, -1, -1]
+
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+
+from sklearn.naive_bayes import MultinomialNB
+
+modelo = MultinomialNB()
+modelo.fit(dados, marcacoes)
+
+teste = [misterioso1, misterioso2]
+
+print(modelo.predict(teste))
