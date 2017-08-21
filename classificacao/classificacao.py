093d467c65b2fc90dd96107c95dd4217093a349a
rmzoni <rmzoni@gmail.com>
2017-08-20 23:00:27 -0300

    Utilizando variáveis categorica e modulo pandas

--- classificacao/classificacao.py
+++ classificacao/classificacao.py
@@ -12,12 +12,25 @@ marcacoes = [1, 1, 1, -1, -1, -1]
 
 misterioso1 = [1, 1, 1]
 misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
 
 from sklearn.naive_bayes import MultinomialNB
 
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
 
-teste = [misterioso1, misterioso2]
+teste = [misterioso1, misterioso2, misterioso3]
+marcacoes_teste = [-1, 1, -1]
 
-print(modelo.predict(teste))
+resultado = modelo.predict(teste)
+diferencas = resultado - marcacoes_teste
+
+acertos = [d for d in diferencas if d == 0]
+total_de_acertos = len(acertos)
+total_de_elementos = len(teste)
+
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
+
+print(resultado)
+print(diferencas)
+print(taxa_de_acerto)
